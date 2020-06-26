deeecc757e7274a118a13c0f548512af8983f79d
Brian <villarroelbrian1110@gmail.com>
2020-06-26 07:59:34 -0300

    se finaliza logica de busqueda

--- js/app.js
+++ js/app.js
@@ -2,14 +2,31 @@ console.log("hola");
 
 const apiKey = "TMGSxnWlK9PpInWuG6JMNRCvys8lrEbb";
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * main
+ */
+(()=>{
+    console.log("inicio")
+    document.getElementById("btnBuscar").disabled =true;
+    obtenerResultadoSugenrencias("post malone");
+    obtenerResultadoTendencias("tendencias");
+    
+    console.log(document.getElementById("estilo").href="./styles/oscuro.css");
+})();
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ * funcion busqueda
+ * @param {*} search 
+ */
 function getSearchResults(search) { 
-    const found = fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + apiKey)
+    const found = fetch(`http://api.giphy.com/v1/gifs/search?q=${search}&limit=18&api_key=${apiKey}`)
     .then((response) => {
         return response.json()
     }).then(data => {
         // return data
-        console.log("resultado de busqueda:");
-        console.log(data);
+        // console.log("resultado de busqueda:");
+        // console.log(data);
         data = data.data;
         let cuadrogif="";
         for (let i = 0; i < data.length; i++) {
@@ -25,19 +42,129 @@ function getSearchResults(search) {
     })
     return found
 }
+/**
+ * funcion obtener sugerencia
+ * @param {*} search 
+ */
+function obtenerResultadoSugenrencias(search) { 
+    const found = fetch(`http://api.giphy.com/v1/gifs/search?q=${search}&limit=6&api_key=${apiKey}`)
+    .then((response) => {
+        return response.json()
+    }).then(data => {
+        // console.log("resultado de busqueda sugerencia:");
+        // console.log(data);
+        data = data.data;
+        let cuadrogif="";
+        for (let i = 0; i < data.length; i++) {
+            cuadrogif+= "<div class='cuadro-fig'>";
+            cuadrogif+= `<img src="${data[i].images.original.url}" alt=""/>`;
+            cuadrogif+= `<div class='cuadro-fig-titulo'> "${data[i].title}" </div>`;
+            cuadrogif+= "</div>";
+        }
+        document.getElementById("resultadoSugerencia").innerHTML = cuadrogif;
+    }).catch((error) => {
+        return error
+    })
+    return found
+}
 
+/**
+ * funcion obtener tendencias
+ * @param {*} search 
+ */
+function obtenerResultadoTendencias(search) { 
+    const found = fetch(`http://api.giphy.com/v1/gifs/search?q=${search}&limit=6&api_key=${apiKey}`)
+    .then((response) => {
+        return response.json()
+    }).then(data => {
+        // console.log("resultado de busqueda sugerencia:");
+        // console.log(data);
+        data = data.data;
+        let cuadrogif="";
+        for (let i = 0; i < data.length; i++) {
+            cuadrogif+= "<div class='cuadro-fig'>";
+            cuadrogif+= `<img src="${data[i].images.original.url}" alt=""/>`;
+            cuadrogif+= `<div class='cuadro-fig-titulo'> "${data[i].title}" </div>`;
+            cuadrogif+= "</div>";
+        }
+        document.getElementById("resultadoTendencia").innerHTML = cuadrogif;
+    }).catch((error) => {
+        return error
+    })
+    return found
+}
+
+//https://api.giphy.com/v1/gifs/search/tags?q=
+/**
+ * funcion autocompletado
+ * @param {*} search 
+ */
+function obtenerResultadoAutocompletado(search) { 
+    const found = fetch(`https://api.giphy.com/v1/gifs/search/tags?q=${search}&limit=3&api_key=${apiKey}`)
+    .then((response) => {
+        return response.json()
+    }).then(data => {
+        // console.log("resultado de busqueda sugerencia:");
+        // console.log(data);
+        data = data.data;
+        let itemAutoCompletado="";
+        for (let i = 0; i < data.length; i++) {
+            itemAutoCompletado+=`<li id="itemAutocompletado" onClick="eventoClickItemAutocompletado(event)">${data[i].name}</li>`; 
+        }
+        document.getElementById("resultadoAutocompletado").innerHTML = itemAutoCompletado;
+    }).catch((error) => {
+        return error
+    })
+    return found
+}
 
 //eventos
-
+//busqueda
 document.getElementById("btnBuscar").addEventListener("click",()=>{
     let valorBusqueda = document.getElementById("txtBusqueda").value;
     getSearchResults(valorBusqueda);//tituloBusqueda
-    document.getElementById("tituloBusqueda").innerText = valorBusqueda;
+    prepararMuestraDeResultados(valorBusqueda);
+    console.log(`busqueda: ${valorBusqueda}`)
+});
+
+//keyup busqueda
+document.getElementById("txtBusqueda").addEventListener("keyup",()=>{
+    let valorBusqueda = document.getElementById("txtBusqueda").value;
+    if(valorBusqueda.trim()!=""){
+        document.getElementById("btnBuscar").disabled= false ;
+        document.getElementsByClassName("cuadro-busqueda-resultados")[0].style.display="block";
+    }
+    else
+    {
+        document.getElementsByClassName("cuadro-busqueda-resultados")[0].style.display="none";
+        document.getElementById("btnBuscar").disabled= true ;
+    }
+    // console.log(valorBusqueda);
+    // console.log("resultado auto completado:")
+    obtenerResultadoAutocompletado(valorBusqueda);
+    
+});
+
+//click itemAutocompletado
+function eventoClickItemAutocompletado(event){
+    // console.log("resultado click itemAutocompletado:");
+    // console.log(event.target.innerText);
+    getSearchResults(event.target.innerText);
+    prepararMuestraDeResultados(event.target.innerText);
+
+}
+
+/**
+ * funcion quita elementos del dom para mostrar unicamente el resultado
+ * @param {*} titulo : texto que se visualizara como encabezado de la busqueda
+ */
+function prepararMuestraDeResultados(titulo){
     document.getElementById("sugerencia").style.display="none";
     document.getElementById("tendencia").style.display="none";
     document.getElementById("resultadoBusqueda").style.display="block";
-    console.log(`busqueda: ${valorBusqueda}`)
-});
+    document.getElementById("tituloBusqueda").innerText= titulo;
+}
+
 
 
 
