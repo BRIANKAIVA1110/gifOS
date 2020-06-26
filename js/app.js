145ad9eaa16ae7a16cc96634c2300052a702c1f3
Brian <villarroelbrian1110@gmail.com>
2020-06-26 04:38:06 -0300

    se finaliza aside resultado busqueda

--- js/app.js
+++ js/app.js
@@ -8,14 +8,18 @@ function getSearchResults(search) {
         return response.json()
     }).then(data => {
         // return data
+        console.log("resultado de busqueda:");
+        console.log(data);
         data = data.data;
         let cuadrogif="";
         for (let i = 0; i < data.length; i++) {
             cuadrogif+= "<div class='cuadro-fig'>";
-            cuadrogif+= `<img src="${data[i].images.original.url}" alt="">`;
+            cuadrogif+= `<img src="${data[i].images.original.url}" alt=""/>`;
+            cuadrogif+= `<div class='cuadro-fig-titulo'> "${data[i].title}" </div>`;
             cuadrogif+= "</div>"
         }
         document.getElementById("resltadoContenedor").innerHTML=cuadrogif;
+        
     }).catch((error) => {
         return error
     })
@@ -27,7 +31,11 @@ function getSearchResults(search) {
 
 document.getElementById("btnBuscar").addEventListener("click",()=>{
     let valorBusqueda = document.getElementById("txtBusqueda").value;
-    getSearchResults(valorBusqueda);
+    getSearchResults(valorBusqueda);//tituloBusqueda
+    document.getElementById("tituloBusqueda").innerText = valorBusqueda;
+    document.getElementById("sugerencia").style.display="none";
+    document.getElementById("tendencia").style.display="none";
+    document.getElementById("resultadoBusqueda").style.display="block";
     console.log(`busqueda: ${valorBusqueda}`)
 });
 
