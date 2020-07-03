0bc07db38815aa99c341517423ab9df63451bb58
Brian <villarroelbrian1110@gmail.com>
2020-07-03 07:41:00 -0300

    se finaliza vista crearGif

--- js/crearGif.js
+++ js/crearGif.js
@@ -33,18 +33,33 @@ function getStreamAndRecord () {
 
 
 document.getElementById("btnGrabar").addEventListener("click",()=>{
+    document.getElementById("video").style.display="block";
+    document.getElementById("contenedorGif").style.display="none";
     getStreamAndRecord();
 });
 
 document.getElementById("btnStop").addEventListener("click",()=>{
-    let result="";
+    
     //funalizo recorder y le defino que hacer con el resultado(lo que se "grabo")
     recorder.stopRecording(function(){
-        let blob = recorder.getBlob();
+        let result="";
+        blob = recorder.getBlob();
         //blod = Binary Large Object
         //URL.createObjectURL -> recibe parametros blod o file-->(binarios). representa binarios en una url?
         result+= `<img id="contenedorGifGrabacion" src=${URL.createObjectURL(blob)} alt='giff'/>`;
+        document.getElementById("video").style.display="none";
+        document.getElementById("contenedorGif").style.display="block";
         document.getElementById("contenedorGif").innerHTML= result;
     });
     
+});
+
+//se muestra el "formulario de grabacion"
+document.getElementById("btnComenzar").addEventListener("click",()=>{
+    document.getElementById("panelVentanaTerminos").style.display="none";
+    document.getElementById("panelVentanaGrabacion").style.display="block";
+});
+
+document.getElementById("btnCancelar").addEventListener("click",()=>{
+    location.href="../index.html";
 });
